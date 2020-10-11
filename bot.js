fd1e656541680344c1bcef21dc212ce7120d54ce
Tony <62971605+TonnieDev@users.noreply.github.com>
2020-10-11 08:08:22 -0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    token: "NzY0Njk5OTA5OTk0OTcxMTM2.X4KESQ.RZhLPIhp-C7gaT2On_XT2CyLjCY", //Token bot
+
+    idbot: "764699909994971136", //Bot ID
+
+    prefix: "g?", //Bot prefix
+
+…        support: "XXX", //The link of your Discord server
+        dperms: "8" //The permissions that the bot asks on we want to add it on a Discord server (8 = moderator)
+    },
+};
